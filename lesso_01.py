edb395736d5b031a67f607f5b3b2ec2060ac747f
First_PR_Pudovkin_133.2 <pudovkinniko@gmail.com>
2026-06-03 21:56:06 +0400

    Add files via upload

---
+++ lesso_01.py
@@ -0,0 +1,27 @@
+my_heigh=180
+print(my_heigh)
+
+my_name = "Иван"
+my_name = "Иван Петров"
+print(my_name)
+
+pet_name = ("Алиса ")
+print("Ваш любимчик - " + pet_name)
+
+def print_python():
+    print("Учу Python!")
+
+
+print_python()
+
+def print_letter(let):
+    print(let, end='')
+
+
+print_letter('C')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
