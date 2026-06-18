69dda8c7ae3315e7c4811028efa663c9f7e2174a
pudovkinniko-dotcom <pudovkinniko@gmail.com>
2026-06-18 21:39:55 +0400

    03_lesson

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -15,6 +15,10 @@ mailing = Mailing(
 
 # Выводим информацию в консоль
 print(f"Отправление {mailing.track} из "
-      f"{mailing.from_address.index}, {mailing.from_address.city}, {mailing.from_address.street}, {mailing.from_address.house} - {mailing.from_address.apartment} в "
-      f"{mailing.to_address.index}, {mailing.to_address.city}, {mailing.to_address.street}, {mailing.to_address.house} - {mailing.to_address.apartment}. "
+      f"{mailing.from_address.index}, {mailing.from_address.city},"
+      f"{mailing.from_address.street}, {mailing.from_address.house} - "
+      f"{mailing.from_address.apartment} в "
+      f"{mailing.to_address.index}, {mailing.to_address.city}, "
+      f"{mailing.to_address.street}, {mailing.to_address.house} - "
+      f"{mailing.to_address.apartment}. "
       f"Стоимость {mailing.cost} рублей.")
--- 03_lesson/mailing.py
+++ 03_lesson/mailing.py
@@ -1,7 +1,9 @@
 from address import Address
 
+
 class Mailing:
-    def __init__(self, to_address: Address, from_address: Address, cost: float, track: str):
+    def __init__(self, to_address: Address, from_address: Address,
+                 cost: float, track: str):
         self.to_address = to_address
         self.from_address = from_address
         self.cost = cost
