649eaea5ceaa0b46438cdb3bb3c2aae8c0077789
pudovkinniko-dotcom <pudovkinniko@gmail.com>
2026-06-26 20:55:18 +0400

    3

---
+++ 03_lesson/lesson_3_task_1.py
@@ -0,0 +1,9 @@
+from user import User
+
+# Создаем экземпляр класса и сохраняем в переменную my_user
+my_user = User("Николай", "Пудовкин")
+
+# Вызываем все три метода
+my_user.print_first_name()
+my_user.print_last_name()
+my_user.print_full_name()
